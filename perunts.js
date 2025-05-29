c05b79ccac97ea6e7bffc1b92cf89c0ca79584e4
kayquestreser <kayque.stresser@escola.pr.gov.br>
2025-05-29 08:29:15 -0300

    Add files via upload

---
+++ perunts.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'recepçao ',
+    'O que é manchete?',
+    'a manchete e um fundamento do volei'
+)
+
+criaCartao(
+    'ataque',
+    'Qual o nome do jogador que formula as jogadas?',
+    'o levantador e o quem fomula as jogadas em quadra'
+)
+
+criaCartao(
+    'ataque',
+    'qual o nome do principal ataque do volei?',
+    'saque e um dos principais ataques no volei '
+)
+
+criaCartao(
+    'bloqueio',
+    'qual o nome quando o jogador toca na bola no bloqueio?',
+    'tuch ou toque '
+)
